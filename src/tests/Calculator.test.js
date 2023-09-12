099572774cb6c2318901be77f96626aea40df52c
WeslleiBrito <82123233+WeslleiBrito@users.noreply.github.com>
2023-09-12 12:39:31 -0300

    Exercicio 2

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,6 +1,6 @@
 import { screen, render } from "@testing-library/react" 
 import Calculator from "../components/Calculator"
-
+import userEvent from "@testing-library/user-event"
 
 describe("Testando o calculator", () => {
 
@@ -13,9 +13,33 @@ describe("Testando o calculator", () => {
         const multiply = screen.getByText('*')
         const divide = screen.getByText('/')
 
-        expect(add).toBeInTheDocument('+')
-        expect(subtract).toBeInTheDocument('-')
-        expect(multiply).toBeInTheDocument('*')
-        expect(divide).toBeInTheDocument('/')
+        expect(add).toBeInTheDocument()
+        expect(subtract).toBeInTheDocument()
+        expect(multiply).toBeInTheDocument()
+        expect(divide).toBeInTheDocument()
+    })
+
+    test("Deve efetuar corretamente a multiplicação de 5 * 2",  async () => {
+
+        const user = userEvent.setup()
+
+        render(<Calculator/>)
+
+        screen.logTestingPlaygroundURL()
+
+        const five = screen.getByRole('button', { name: /5/i})
+        const tow = screen.getByRole('button', {name: /2/i})
+        const multiply = screen.getByText('*')
+        const equal = screen.getByRole('button', {name: /=/i})
+
+        await user.click(five)
+        await user.click(multiply)
+        await user.click(tow)
+        await user.click(equal)
+
+        const result = screen.getByText(/10/i)
+
+        expect(result).toBeInTheDocument()
+
     })
 })
